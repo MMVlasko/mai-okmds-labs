643d113d41ed69ee48940235550890ad52c5cedf
MMVlasko <mmvlasko@mai.education>
2024-12-01 16:56:05 +0300

    fiz all

--- LW1/solution.py
+++ LW1/solution.py
@@ -6,47 +6,65 @@ import math
 
 
 def rot2d(_x, _y, angle):
+    """Преобразование данных координат
+    на соответствующий угол поворота"""
     return (
         _x * np.cos(angle) - _y * np.sin(angle),
         _x * np.sin(angle) + _y * np.cos(angle)
     )
 
 
-def mdl(data, i):
-    return round((data[i] ** 2 + data[i] ** 2) ** 0.5, 5)
+def mdl(data_x, data_y, i):
+    """Получение модуля вектора на i-ом шаге"""
+    return round((data_x[i] ** 2 + data_y[i] ** 2) ** 0.5, 5)
 
 
+# Вводим символьную переменную
 t = sp.Symbol('t')
 
+# Получаем уравнения изменения x и y по t
 x = (1 + sp.sin(5 * t)) * sp.cos(t)
 y = (1 + sp.sin(5 * t)) * sp.sin(t)
 
+# Получаем скорость по x и y, а также модуль
 Vx = sp.diff(x, t)
 Vy = sp.diff(y, t)
 V = (Vx ** 2 + Vy ** 2) ** 0.5
 
+# То же для полного ускорения
 Wx = sp.diff(Vx, t)
 Wy = sp.diff(Vy, t)
 W = (Wx ** 2 + Wy ** 2) ** 0.5
 
+# То же для тангенциального ускорения
 Wt = sp.diff(V, t)
+
+# То же для нормального ускорения
 Wn = (W ** 2 - Wt ** 2) ** 0.5
 
+# Проекции тангенциального ускорения
 Wtx = Vx / V * Wt
 Wty = Vy / V * Wt
 
+# Проекции нормального ускорения
 Wnx = Wx - Wtx
 Wny = Wy - Wty
 
+# Единичные направления нормального ускорения
 Nx = Wnx / Wn
 Ny = Wny / Wn
 
+# Радиус кривизны
 curvature_module = ((V * V) / Wn)
+
+# Его проекции
 curvature_x = curvature_module * Nx
 curvature_y = curvature_module * Ny
 
+# Массив временных точек
 T = np.linspace(0, 6.28, 1000)
 
+# Получение функций изменения величин
 Fx = sp.lambdify(t, x, 'numpy')
 Fy = sp.lambdify(t, y, 'numpy')
 
@@ -59,62 +77,81 @@ FWy = sp.lambdify(t, Wy, 'numpy')
 F_curvature_x = sp.lambdify(t, curvature_x, 'numpy')
 F_curvature_y = sp.lambdify(t, curvature_y, 'numpy')
 
+# Массивы координат
 X = Fx(T)
 Y = Fy(T)
 
+# Отмасштабированные массивы скорости и ускорения
 VX = FVx(T) * 0.2
 VY = FVy(T) * 0.2
 
 WX = FWx(T) * 0.05
 WY = FWy(T) * 0.05
 
+# Массивы радиуса кривизны
 CX = F_curvature_x(T)
 CY = F_curvature_y(T)
 
+# Инициализация pyplot
 figure = plt.figure()
 figure.canvas.manager.set_window_title('Вариант 2')
 
+# Настройка окна отображения графика
 axis = figure.add_subplot(1, 1, 1)
 axis.axis('equal')
 axis.set(ylim=[-2.5, 3])
 
+# Отрисовка материальной точки
 axis.plot(X, Y, 'black')
 point = axis.plot(X[0], Y[0], marker='o', markerfacecolor='grey', markeredgecolor='red', markersize=9)[0]
 
+# Отрисовка вектора скорости
 v_line = axis.plot([X[0], X[0] + VX[0]], [Y[0], Y[0] + VY[0]], 'r', label='Скорость (V)')[0]
 
+# Массивы координат стрелки вектора скорости
 x_v_arrow = np.array([-0.05, 0, -0.05])
 y_v_arrow = np.array([0.05, 0, -0.05])
 
+# Стартовый поворот стрелки и её отрисовка
 r_x_v_arrow, r_y_v_arrow = rot2d(x_v_arrow, y_v_arrow, math.atan2(VY[0], VX[0]))
 v_arrow = axis.plot(r_x_v_arrow + X[0] + VX[0], r_y_v_arrow + Y[0] + VY[0], 'r')[0]
 
+# Отрисовка вектора ускорения
 w_line = axis.plot([X[0], X[0] + WX[0]], [Y[0], Y[0] + WY[0]], 'g', label='Ускорение (W)')[0]
 
+# Массивы координат стрелки вектора ускорения
 x_w_arrow = np.array([-0.05, 0, -0.05])
 y_w_arrow = np.array([0.05, 0, -0.05])
 
+# Стартовый поворот стрелки и её отрисовка
 r_x_w_arrow, r_y_w_arrow = rot2d(x_w_arrow, y_w_arrow, math.atan2(WY[0], WX[0]))
 w_arrow = axis.plot(r_x_w_arrow + X[0] + WX[0], r_y_w_arrow + Y[0] + WY[0], 'g')[0]
 
+# Отрисовка радиус-вектора
 r_line = axis.plot([0, X[0]], [0, Y[0]], 'b', label='Радиус-вектор (R)')[0]
 
+# Массивы координат стрелки радиус-вектора
 x_r_arrow = np.array([-0.05, 0, -0.05])
 y_r_arrow = np.array([0.05, 0, -0.05])
 
+# Стартовый поворот стрелки и её отрисовка
 r_x_r_arrow, r_y_r_arrow = rot2d(x_r_arrow, y_r_arrow, math.atan2(Y[0], X[0]))
 r_arrow = axis.plot(r_x_r_arrow + X[0], r_y_w_arrow + Y[0], 'b')[0]
 
+# Отрисовка радиуса кривизны
 curvature_radius = axis.plot([X[0], X[0] + CX[0]], [Y[0], Y[0] + CY[0]], 'black', label='Радиус кривизны (CR)',
                              linestyle='--')[0]
 
+# Шаблон сообщения со значениями величин
 raw_text = 'R   = {r}\nV   = {v}\nW  = {w}\nCR = {cr}'
 
-text = axis.text(0.03, 0.03, raw_text.format(r=mdl(X, 0), v=mdl(VX, 0), w=mdl(WX, 0), cr=mdl(CX, 0)),
-                 transform=axis.transAxes, fontsize=8)
+# Установка сообщения в начальное состояние
+text = axis.text(0.03, 0.03, raw_text.format(r=mdl(X, Y, 0), v=mdl(VX, VY, 0), w=mdl(WX, WY, 0),
+                                             cr=mdl(CX, CY, 0)), transform=axis.transAxes, fontsize=8)
 
 
 def animate(i):
+    """Изменение величин для нового шага анимации"""
     global r_y_v_arrow, r_x_v_arrow, r_x_w_arrow, r_y_w_arrow, r_x_r_arrow, r_y_r_arrow
 
     point.set_data(X[i], Y[i])
@@ -133,13 +170,16 @@ def animate(i):
 
     curvature_radius.set_data([X[i], X[i] + CX[i]], [Y[i], Y[i] + CY[i]])
 
-    text.set_text(raw_text.format(r=mdl(X, i), v=mdl(VX, i), w=mdl(WX, i), cr=mdl(CX, i)))
+    text.set_text(raw_text.format(r=mdl(X, Y, i), v=mdl(VX, VY, i), w=mdl(WX, WY, i), cr=mdl(CX, CY, i)))
 
     return point, v_line, v_arrow, curvature_radius
 
 
+# Запуск анимации
 animation = FuncAnimation(figure, animate, frames=1000, interval=1)
 
+# Вывод легенды
 axis.legend(fontsize=7)
 
+# Запуск pyplot
 plt.show()
--- LW2/solution.py
+++ LW2/solution.py
@@ -3,68 +3,105 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+# Количество шагов анимации
 STEPS = 1001
+
+# Продолжительность анимации
 FINAL_TIME = 20
+
+# Длина тележки
 BOX_X = 6
+
+# Высота тележки
 BOX_Y = 2
-SPR_X_0 = 4
+
+# Длина пружины в недеформированном состоянии
+X0 = 4
+
+# Радиус колеса
 WHEEL_R = 0.5
-LENGTH = 1.85
+
+# Длина стержня
+LENGTH = 3
 BLACK = 'black'
 RED = 'red'
 
+# Массив временных точек
 t = np.linspace(0, FINAL_TIME, STEPS)
 
+# Выражения для изменения угла и координаты
 x = np.cos(0.2 * t) + 3 * np.sin(1.8 * t)
 phi = 2 * np.sin(1.7 * t) + 5 * np.cos(1.2 * t)
 
-x_a = SPR_X_0 + x + BOX_X / 2
+# Выражения координат центра тележки
+x_a = X0 + x + BOX_X / 2
 y_a = 2 * WHEEL_R + BOX_Y / 2 + 0.2
 
+# Выражения координат конца стержня
 x_b = x_a + LENGTH * np.sin(phi)
 y_b = y_a + LENGTH * np.cos(phi)
 
+# Массивы относительных координат углов тележки для отрисовки
 x_box = np.array([-BOX_X / 2, BOX_X / 2, BOX_X / 2, -BOX_X / 2, -BOX_X / 2])
 y_box = np.array([BOX_Y / 2, BOX_Y / 2, -BOX_Y / 2, -BOX_Y / 2, BOX_Y / 2])
 
+# Массив углов и выражения для отрисовки окружностей колёс
 psi = np.linspace(0, 2 * math.pi, 30)
 x_wheel = WHEEL_R * np.cos(psi)
 y_wheel = WHEEL_R * np.sin(psi)
 
-x_c1 = SPR_X_0 + x + BOX_X / 5
+# Выражения для координат центров колёс
+x_c1 = X0 + x + BOX_X / 5
 y_c1 = WHEEL_R
-x_c2 = SPR_X_0 + x + 4 * BOX_X / 5
+x_c2 = X0 + x + 4 * BOX_X / 5
 y_c2 = WHEEL_R
 
+# Координаты отрисовки координатных осей
 x_ground = np.array([0, 0, 15])
 y_ground = np.array([6, 0, 0])
 
+# Число точек перегиба пружины
 K = 19
+
+# Относительная высота точки перегиба пружины
 SH = 0.4
+
+# Расстояние между двумя точками перегиба (кроме крайних)
 B = 1 / (K - 2)
+
+# Инициализация массивов координат точек перегиба
 x_spr = np.zeros(K)
 y_spr = np.zeros(K)
-x_spr[0] = 0
-y_spr[0] = 0
-x_spr[K - 1] = 1
-y_spr[K - 1] = 0
 
+# Начальная координата крайней правой точки
+x_spr[K - 1] = 1
+
+# Заполнение массивов координатами
 for j in range(K - 2):
     x_spr[j + 1] = B * ((j + 1) - 1 / 2)
     y_spr[j + 1] = SH * (-1) ** j
 
-spright_length = SPR_X_0 + x
+# Массив длин пружины в разные моменты
+spright_length = X0 + x
 
+# Количество полных витков
 NV = 3
-R1 = 0.05
-R2 = 0.8
 
+# Минимальный радиус пружины
+R1 = 0.2
+
+# Максимальный радиус пружины
+R2 = 0.9
+
+# Получения массива углов и координат точек спиральной пружины
 theta = np.linspace(0, NV * 2 * math.pi - phi[0], 100)
 x_spiral_spr = -(R1 + theta * (R2 - R1) / theta[-1]) * np.sin(theta)
 y_spiral_spr = (R1 + theta * (R2 - R1) / theta[-1]) * np.cos(theta)
 
+# Массив углов вращения колёс
 alpha = x / WHEEL_R
 
+# Инициализация окна с графиком
 figure = plt.figure(figsize=[10, 5])
 figure.canvas.manager.set_window_title('Вариант 2')
 
@@ -72,59 +109,71 @@ axis = figure.add_subplot(1, 1, 1)
 axis.axis('equal')
 axis.set(xlim=[0, 15], ylim=[-1.5, 6.5])
 
+# Отрисовка координатных осей
 axis.plot(x_ground, y_ground, BLACK)
 
-drew_box = axis.plot(x_box + x_a[0], y_box + y_a, BLACK)[0]
-drew_wheel1 = axis.plot(x_wheel + x_c1[0], y_wheel + y_c1, BLACK)[0]
-drew_wheel2 = axis.plot(x_wheel + x_c2[0], y_wheel + y_c2, BLACK)[0]
+# Отрисовка тележки и колёс
+drawn_box = axis.plot(x_box + x_a[0], y_box + y_a, BLACK)[0]
+drawn_wheel1 = axis.plot(x_wheel + x_c1[0], y_wheel + y_c1, BLACK)[0]
+drawn_wheel2 = axis.plot(x_wheel + x_c2[0], y_wheel + y_c2, BLACK)[0]
 
-drew_vc1 = axis.plot([x_c1[0] - WHEEL_R, x_c1[0], x_c1[0] + WHEEL_R], [2 * y_c1 + 0.2, y_c1, 2 * y_c1 + 0.2], BLACK)[0]
-drew_vc2 = axis.plot([x_c2[0] - WHEEL_R, x_c2[0], x_c2[0] + WHEEL_R], [2 * y_c2 + 0.2, y_c2, 2 * y_c2 + 0.2], BLACK)[0]
+# Отрисовка креплений колёс
+drawn_vc1 = axis.plot([x_c1[0] - WHEEL_R, x_c1[0], x_c1[0] + WHEEL_R], [2 * y_c1 + 0.2, y_c1, 2 * y_c1 + 0.2], BLACK)[0]
+drawn_vc2 = axis.plot([x_c2[0] - WHEEL_R, x_c2[0], x_c2[0] + WHEEL_R], [2 * y_c2 + 0.2, y_c2, 2 * y_c2 + 0.2], BLACK)[0]
 
-drew_spring = axis.plot(x_spr * spright_length[0], y_spr + y_a, RED)[0]
-drew_spiral_spring = axis.plot(x_spiral_spr + x_a[0], y_spiral_spr + y_a, RED)[0]
+# Отрисовка обычной пружины
+drawn_spring = axis.plot(x_spr * spright_length[0], y_spr + y_a, RED)[0]
 
-drew_wheel1_d1 = axis.plot([x_c1[0] + WHEEL_R * np.sin(alpha[0]), x_c1[0] - WHEEL_R * np.sin(alpha[0])],
+# Отрисовка спиральной пружины
+drawn_spiral_spring = axis.plot(x_spiral_spr + x_a[0], y_spiral_spr + y_a, RED)[0]
+
+# Отрисовка диаметров колёс
+drawn_wheel1_d = axis.plot([x_c1[0] + WHEEL_R * np.sin(alpha[0]), x_c1[0] - WHEEL_R * np.sin(alpha[0])],
                            [y_c1 + WHEEL_R * np.cos(alpha[0]), y_c1 - WHEEL_R * np.cos(alpha[0])], BLACK)[0]
-drew_wheel2_d2 = axis.plot([x_c2[0] + WHEEL_R * np.sin(alpha[0]), x_c2[0] - WHEEL_R * np.sin(alpha[0])],
+drawn_wheel2_d = axis.plot([x_c2[0] + WHEEL_R * np.sin(alpha[0]), x_c2[0] - WHEEL_R * np.sin(alpha[0])],
                            [y_c2 + WHEEL_R * np.cos(alpha[0]), y_c2 - WHEEL_R * np.cos(alpha[0])], BLACK)[0]
 
-drew_ab = axis.plot([x_a[0], x_b[0]], [y_a, y_b[0]], BLACK)[0]
+# Отрисовка стержня
+drawn_ab = axis.plot([x_a[0], x_b[0]], [y_a, y_b[0]], BLACK)[0]
 
+# Отрисовка шарнира и груза на конце стержня
 point_a = axis.plot(x_a[0], y_a, marker='o')[0]
 point_b = axis.plot(x_b[0], y_b[0], marker='o', markersize=20)[0]
 
 
 def animate(i):
-    drew_box.set_data(x_box + x_a[i], y_box + y_a)
+    """Изменение величин для нового шага анимации"""
+    drawn_box.set_data(x_box + x_a[i], y_box + y_a)
 
-    drew_wheel1.set_data(x_wheel + x_c1[i], y_wheel + y_c1)
-    drew_wheel2.set_data(x_wheel + x_c2[i], y_wheel + y_c2)
+    drawn_wheel1.set_data(x_wheel + x_c1[i], y_wheel + y_c1)
+    drawn_wheel2.set_data(x_wheel + x_c2[i], y_wheel + y_c2)
 
-    drew_vc1.set_data([x_c1[i] - WHEEL_R, x_c1[i], x_c1[i] + WHEEL_R], [2 * y_c1 + 0.2, y_c1, 2 * y_c1 + 0.2])
-    drew_vc2.set_data([x_c2[i] - WHEEL_R, x_c2[i], x_c2[i] + WHEEL_R], [2 * y_c2 + 0.2, y_c2, 2 * y_c2 + 0.2])
+    drawn_vc1.set_data([x_c1[i] - WHEEL_R, x_c1[i], x_c1[i] + WHEEL_R], [2 * y_c1 + 0.2, y_c1, 2 * y_c1 + 0.2])
+    drawn_vc2.set_data([x_c2[i] - WHEEL_R, x_c2[i], x_c2[i] + WHEEL_R], [2 * y_c2 + 0.2, y_c2, 2 * y_c2 + 0.2])
 
-    drew_ab.set_data([x_a[i], x_b[i]], [y_a, y_b[i]])
+    drawn_ab.set_data([x_a[i], x_b[i]], [y_a, y_b[i]])
     point_a.set_data(x_a[i], y_a)
     point_b.set_data(x_b[i], y_b[i])
 
-    drew_spring.set_data(x_spr * spright_length[i], y_spr + y_a)
+    drawn_spring.set_data(x_spr * spright_length[i], y_spr + y_a)
     _theta = np.linspace(0, NV * 2 * math.pi - phi[i], 100)
     _x_spiral_spr = -(R1 + _theta * (R2 - R1) / _theta[-1]) * np.sin(_theta)
     _y_spiral_spr = (R1 + _theta * (R2 - R1) / _theta[-1]) * np.cos(_theta)
-    drew_spiral_spring.set_data(_x_spiral_spr + x_a[i], _y_spiral_spr + y_a)
+    drawn_spiral_spring.set_data(_x_spiral_spr + x_a[i], _y_spiral_spr + y_a)
 
-    drew_wheel1_d1.set_data([x_c1[i] + WHEEL_R * np.sin(alpha[i]), x_c1[i] - WHEEL_R * np.sin(alpha[i])],
+    drawn_wheel1_d.set_data([x_c1[i] + WHEEL_R * np.sin(alpha[i]), x_c1[i] - WHEEL_R * np.sin(alpha[i])],
                             [y_c1 + WHEEL_R * np.cos(alpha[i]), y_c1 - WHEEL_R * np.cos(alpha[i])])
-    drew_wheel2_d2.set_data([x_c2[i] + WHEEL_R * np.sin(alpha[i]), x_c2[i] - WHEEL_R * np.sin(alpha[i])],
+    drawn_wheel2_d.set_data([x_c2[i] + WHEEL_R * np.sin(alpha[i]), x_c2[i] - WHEEL_R * np.sin(alpha[i])],
                             [y_c2 + WHEEL_R * np.cos(alpha[i]), y_c2 - WHEEL_R * np.cos(alpha[i])])
 
     return (
-        drew_box, drew_wheel1, drew_wheel2, drew_vc1, drew_vc2, drew_ab, point_a,
-        point_b, drew_spring, drew_spiral_spring, drew_wheel1_d1, drew_wheel2_d2
+        drawn_box, drawn_wheel1, drawn_wheel2, drawn_vc1, drawn_vc2, drawn_ab, point_a,
+        point_b, drawn_spring, drawn_spiral_spring, drawn_wheel1_d, drawn_wheel2_d
     )
 
 
+# Запуск анимации
 anim = FuncAnimation(figure, animate, frames=len(t), interval=40, repeat=False)
 
+# Запуск pyplot
 plt.show()
